cc6bf1b560551042b74bc5726008603e70d72503
Markus Tacker <m@coderbyheart.de>
2013-01-31 14:20:35 +0100

    Kleines Update am erzeugten XML.

--- rheinmainrocks/pipelines.py
+++ rheinmainrocks/pipelines.py
@@ -1,12 +1,21 @@
 import re
+import os
 
 class RheinmainrocksPipeline(object):
     def process_item(self, item, spider):
-        filename=re.sub("[^A-Za-z0-9]", "", unicode(item["name"]).lower()) + ".xml"
+        nickname = re.sub("[^A-Za-z0-9]", "", unicode(item["name"]).lower())
+        filename=nickname + ".xml"
         with open(filename, "w") as x:
-            x.write('<?xml version="1.0" encoding="UTF-8"?>')
-            x.write('<usergroup>')
-            for k in ['name','url','location','subject']:
-                x.write('<%s>%s</%s>' % (k, unicode(item[k]), k))
-            x.write('</usergroup>')
+            writeln = lambda s: x.write(s + os.linesep)
+            writeln('<?xml version="1.0" encoding="UTF-8"?>')
+            writeln('<usergroup xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../xsd/usergroup.xsd">')
+            writeln('    <name>%s</name>' % unicode(item['name']))
+            writeln('    <nickname>%s</nickname>' % unicode(nickname))
+            writeln('    <description>%s</description>' % unicode(item['location']))
+            writeln('    <url>%s</url>' % unicode(item['url']))
+            writeln('    <tags>')
+            for tag in unicode(item['subject']).split(','):
+                writeln('        <tag>%s</tag>' % tag.strip())
+            writeln('    </tags>')
+            writeln('</usergroup>')
         return item
